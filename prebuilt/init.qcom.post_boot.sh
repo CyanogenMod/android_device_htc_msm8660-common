7550a2e6b6a081e91d7005ce33be334f6e730cb6
vorbeth <vorbeth@gmail.com>
2012-05-07 12:27:33 +0100

    msm8660-common: allow performance menu to control CPU settings
    Change-Id: I3159901828d4ae9648c491e6fd01860101ff67b8

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -50,14 +50,10 @@ case "$target" in
 	 echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/idle_enabled
 	 echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/idle_enabled
 	 echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/idle_enabled
-	 echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-	 echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 	 echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
 	 echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
 	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
