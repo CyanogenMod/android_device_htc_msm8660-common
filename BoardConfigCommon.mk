142b7726e6779248aa2f53be79977fcb3480c236
Giulio Cervera <giulio.cervera@gmail.com>
2012-06-10 15:24:25 +0200

    msm8660-common: remove wifi module
    Change-Id: Ia91890bd5b3ae5c645e658038d9882c2c2c8e1bc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,9 +51,6 @@ WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_NAME          := bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
-
 # Audio
 COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
 TARGET_USES_QCOM_LPA := true
