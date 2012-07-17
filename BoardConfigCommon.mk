57afeefab66a7cd8b1b96248a8d6b14d2f31d468
cretin45 <cretin45@gmail.com>
2012-07-17 15:17:50 -0700

    Disable LPA for now.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,8 +52,8 @@ WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 # Audio
-COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
-TARGET_USES_QCOM_LPA := true
+#COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+#TARGET_USES_QCOM_LPA := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
