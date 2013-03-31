a0b2773432e932e03c372b512ae4fecee225df04
tbalden <illespal@gmail.com>
2013-03-31 23:42:10 +0200

    build properties: removing unwanted composition type 'dyn'
    It's causing unwanted HWC framebuffer renderings, spamming the
    log. Should fix the issue and no drawback found yet.
    
    Change-Id: I1f58abac44e7cf8c2c38db3f79cee37a0eb1565c

--- msm8660.mk
+++ msm8660.mk
@@ -115,10 +115,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Common build properties
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
-    debug.composition.type=dyn \
     debug.egl.hw=1 \
-    debug.enabletr=true \
-    debug.mdpcomp.maxlayer=0 \
     debug.mdpcomp.logs=0 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
