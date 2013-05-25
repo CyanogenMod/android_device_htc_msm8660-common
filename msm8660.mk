e9f9099f417cb4d1cfed027c45eda9bcad277b86
tbalden <illespal@gmail.com>
2013-05-25 21:21:37 +0200

    configs: removing unneeded audio_policy.conf copy from dev tree
    swapped new audio-caf msm8660 works perfectly with VOIP.
    
    Patchset 2: adding back the policy.conf to product_packages
    
    Change-Id: I7a4a54a6ddc8632c35ab0ee3792ec7a05f4fe1d0

--- msm8660.mk
+++ msm8660.mk
@@ -40,6 +40,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
+    audio_policy.conf \
     audio.primary.msm8660 \
     libaudioutils
 
@@ -94,8 +95,7 @@ PRODUCT_PACKAGES += \
 # Media configuration
 PRODUCT_COPY_FILES += \
     device/htc/msm8660-common/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    device/htc/msm8660-common/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    device/htc/msm8660-common/configs/audio_policy.conf:system/etc/audio_policy.conf
+    device/htc/msm8660-common/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 
 # MSM8660 firmware
